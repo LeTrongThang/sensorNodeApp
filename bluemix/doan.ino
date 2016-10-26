d11e8a35b79c3d571a9303ed7f44e08ae0156809
Trinhleo <hnkt2907@gmail.com>
2016-10-26 08:25:27 +0700

    Add files via upload

---
+++ bluemix/doan.ino
@@ -0,0 +1,141 @@
+
+
+
+/**
+   Helloworld style, connect an ESP8266 to the IBM IoT Foundation
+
+   Author: Ant Elder
+   License: Apache License v2
+*/
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+
+//-------- Customise these values -----------
+const char* ssid = "cntt2";
+const char* password = "0838299605";
+
+#define ORG "42o0m3"
+#define DEVICE_TYPE "NodeMCU_DHT11"
+#define DEVICE_ID "Test1"
+#define TOKEN "Qk71CVHJ8xlm9s82pC"
+
+#define DHTTYPE DHT11   // DHT 11
+
+// DHT Sensor
+const int DHTPin = 5;
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+
+// Temporary variables
+static char celsiusTemp[7];
+static char fahrenheitTemp[7];
+static char humidityTemp[7];
+
+//-------- Customise the above values --------
+
+char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
+char topic[] = "iot-2/evt/status/fmt/json";
+char authMethod[] = "use-token-auth";
+char token[] = TOKEN;
+char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
+
+WiFiClient wifiClient;
+PubSubClient client(server, 1883, NULL, wifiClient);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+
+  Serial.print("Connecting to "); Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  Serial.print("WiFi connected, IP address: "); Serial.println(WiFi.localIP());
+  delay(5);
+
+  dht.begin();
+
+}
+
+int counter = 0;
+void loop() {
+
+  if (!!!client.connected()) {
+    Serial.print("Reconnecting client to ");
+    Serial.println(server);
+    while (!!!client.connect(clientId, authMethod, token)) {
+      Serial.print(".");
+      delay(500);
+    }
+    Serial.println();
+  }
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    strcpy(celsiusTemp, "Failed");
+    strcpy(fahrenheitTemp, "Failed");
+    strcpy(humidityTemp, "Failed");
+  }
+  else {
+    // Computes temperature values in Celsius + Fahrenheit and Humidity
+    float hic = dht.computeHeatIndex(t, h, false);
+    dtostrf(hic, 6, 2, celsiusTemp);
+    float hif = dht.computeHeatIndex(f, h);
+    dtostrf(hif, 6, 2, fahrenheitTemp);
+    dtostrf(h, 6, 2, humidityTemp);
+    // You can delete the following Serial.print's, it's just for debugging purposes
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t Temperature: ");
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.print(f);
+    Serial.print(" *F\t Heat index: ");
+    Serial.print(hic);
+    Serial.print(" *C ");
+    Serial.print(hif);
+    Serial.print(" *F");
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t Temperature: ");
+    Serial.print(t);
+    Serial.print(" *C ");
+    Serial.print(f);
+    Serial.print(" *F\t Heat index: ");
+    Serial.print(hic);
+    Serial.print(" *C ");
+    Serial.print(hif);
+    Serial.println(" *F");
+
+    String payload =  "{\"temp\":";
+    payload += t;
+    payload += ",\"hud\" :";
+    payload += h;
+    payload += ",\"counter\":";
+    payload += counter;
+    payload += "}";
+    Serial.print("Sending payload: ");
+    Serial.println(payload);
+
+    if (client.publish(topic, (char*) payload.c_str())) {
+      Serial.println("Publish ok");
+    } else {
+      Serial.println("Publish failed");
+    }
+  }
+
+  ++counter;
+  delay(5000);
+}
